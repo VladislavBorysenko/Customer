595a078d3429aa7c24e5aaf9773800d92cab40f2
Vladislav <legionyipa@gmail.com>
2019-05-22 11:07:54 +0300

    Add House

--- src/house/Application.java
+++ src/house/Application.java
@@ -1,14 +1,27 @@
 package house;
 
 public class Application {
+    public static final int SUM_OF_ROOM = 2;
+    public static final int MIN_FLOOR = 2;
+    public static final int MAX_FLOOR = 5;
+    public static final double SQUER = 50.;
+
     public static void main(String[] args) {
 
+        House[] houses = new House[12];
+        houses = createArray();
+
+        //   methodOfSortFromRoom(houses, SUM_OF_ROOM);
+        //  methodOfSortFromRoomAndFloor(houses, SUM_OF_ROOM, MIN_FLOOR, MAX_FLOOR);
+        methodOfSquerListofHous(houses, SQUER);
     }
 
     private static House[] createArray() {
+        System.out.println("Method: createArray" + '\n');
+
         House[] houses = new House[12];
         houses[0] = new House(1, 54.0, 2, 2, "Chkalova str.", "fivefloor", 50);
-        houses[1] = new House(2, 35.0,5 , 1, "Naukova aveny.", "ninefloor", 50);
+        houses[1] = new House(2, 35.0, 5, 1, "Naukova aveny.", "ninefloor", 50);
         houses[2] = new House(3, 69.0, 6, 3, "Peremoga str.", "sixstinfloor", 100);
         houses[3] = new House(3, 56.5, 9, 2, "Druzhbi str.", "fivefloor", 75);
         houses[4] = new House(2, 41.7, 1, 1, "Chkalova str.", "ninefloor", 50);
@@ -21,4 +34,36 @@ public class Application {
         houses[11] = new House(1, 45.0, 8, 1, "Druzhbi str.", "fivefloor", 100);
         return houses;
     }
+
+
+    private static void methodOfSortFromRoom(House[] houses, int sumRoom) {
+        System.out.println("Method: methodOfSortFromRoom ");
+
+        for (int i = 0; i < houses.length; i++) {
+            if (houses[i].getNumberOfRooms() == sumRoom) {
+                System.out.println("Sum room is: " + houses[i].getNumberOfRooms() + " Adress is " + houses[i].getAdress() + " Floor is " + houses[i].getFloor());
+            }
+        }
+    }
+
+    private static void methodOfSortFromRoomAndFloor(House[] houses, int sumRoom, int minFloor, int maxFloor) {
+        System.out.println("Method: methodOfSortFromRoomAndFloor");
+
+        for (int i = 0; i < houses.length; i++) {
+            if ((houses[i].getNumberOfRooms() == sumRoom) && (houses[i].getFloor() >= minFloor && houses[i].getFloor() <= maxFloor)) {
+                System.out.println("Sum room is: " + houses[i].getNumberOfRooms() + " Adress is " + houses[i].getAdress() + " Floor is " + houses[i].getFloor());
+            }
+        }
+    }
+
+    private static void methodOfSquerListofHous(House[] houses, double squer) {
+        System.out.println("Method: methodOfSquerListofHous ");
+
+        for (int i = 0; i < houses.length; i++) {
+            if (houses[i].getSquer() > squer) {
+                System.out.println("Squer house is: " + houses[i].getSquer() + " Adress is " + houses[i].getAdress() + " Floor is " + houses[i].getFloor());
+            }
+        }
+    }
+
 }
